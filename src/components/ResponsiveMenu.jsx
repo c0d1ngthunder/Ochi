9146ee6367bd51fa93544ec423646b98e3b6e9de
Nitish Kumar <codingthunder3@gmail.com>
2025-07-11 17:18:40 +0530

    fix: fix responsiveMenu visible on larger screens

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ResponsiveMenu = ({isOpen}) => {
   return (
-    <div>
+    <div className='md:hidden'>
         <div className={`w-full uppercase h-screen fixed -top-full left-0 bg-[#212121] z-50 flex flex-col justify-center transition-all duration-600 ${isOpen ? 'top-0' : '-top-full'}`}>
             <ul className="flex border-t-1 border-gray-500 flex-col gap-4 text-6xl text-white font-[FoundersGrotesk]">
             {["Services", "Our work", "About us", "Insights", "Contact us"].map((link, index) => (
